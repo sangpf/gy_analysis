7c1e6e6d91dc95351bf12ea64458a98660dcbb7f
sangpf <1140936679@qq.com>
2018-04-09 18:08:59 +0800

    fe

--- src/main/java/cn/xdc/controller/TestController.java
+++ src/main/java/cn/xdc/controller/TestController.java
@@ -8,16 +8,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.imageio.stream.FileImageInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 
+@RequestMapping(value = "/user")
 @Controller
 public class TestController {
 
     @RequestMapping(value = "/test.do")
     @ResponseBody
     public AjaxResult Test(HttpServletRequest request){
-
         return AjaxResult.successResult();
+    }
+
+    // ----------------------- 测试 session 共享 ----------------------
+    @RequestMapping("/test_session_add.do")
+    public void test_session_add(HttpServletRequest request){
+        HttpSession session = request.getSession();
+
+        long currentTimeMillis = System.currentTimeMillis();
+        session.setAttribute("wangfei", "王菲在唱歌"+currentTimeMillis);
+    }
+
+    @RequestMapping("/test_session_get.do")
+    public void test_session_get(HttpServletRequest request){
+        HttpSession session = request.getSession();
+
+        Object userName = session.getAttribute("wangfei");
+        System.out.println("--------------------- "+userName);
     }
 
 }
